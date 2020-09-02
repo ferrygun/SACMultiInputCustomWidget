47035e505141c7ec12cc1dc5885cd7f30e4154cb
Ferry Djaja <ferrygun@yahoo.com>
2020-09-02 13:01:40 +0800

    Create multiinput.js

---
+++ multiinput.js
@@ -0,0 +1,434 @@
+(function() {
+    let _shadowRoot;
+    let _id;
+    let _score;
+
+    let div;
+    let widgetName;
+    var Ar = [];
+
+    let tmpl = document.createElement("template");
+    tmpl.innerHTML = `
+      <style>
+      </style>      
+    `;
+
+    class MultiInput extends HTMLElement {
+
+        constructor() {
+            super();
+
+            _shadowRoot = this.attachShadow({
+                mode: "open"
+            });
+            _shadowRoot.appendChild(tmpl.content.cloneNode(true));
+
+            _id = createGuid();
+
+            //_shadowRoot.querySelector("#oView").id = "oView";
+
+            this._export_settings = {};
+            this._export_settings.title = "";
+            this._export_settings.subtitle = "";
+            this._export_settings.icon = "";
+            this._export_settings.unit = "";
+            this._export_settings.footer = "";
+
+            this.addEventListener("click", event => {
+                console.log('click');
+
+            });
+
+            this._firstConnection = 0;
+        }
+
+        connectedCallback() {
+            try {
+                if (window.commonApp) {
+                    let outlineContainer = commonApp.getShell().findElements(true, ele => ele.hasStyleClass && ele.hasStyleClass("sapAppBuildingOutline"))[0]; // sId: "__container0"
+
+                    if (outlineContainer && outlineContainer.getReactProps) {
+                        let parseReactState = state => {
+                            let components = {};
+
+                            let globalState = state.globalState;
+                            let instances = globalState.instances;
+                            let app = instances.app["[{\"app\":\"MAIN_APPLICATION\"}]"];
+                            let names = app.names;
+
+                            for (let key in names) {
+                                let name = names[key];
+
+                                let obj = JSON.parse(key).pop();
+                                let type = Object.keys(obj)[0];
+                                let id = obj[type];
+
+                                components[id] = {
+                                    type: type,
+                                    name: name
+                                };
+                            }
+
+                            for (let componentId in components) {
+                                let component = components[componentId];
+                            }
+
+                            let metadata = JSON.stringify({
+                                components: components,
+                                vars: app.globalVars
+                            });
+
+                            if (metadata != this.metadata) {
+                                this.metadata = metadata;
+
+                                this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                                    detail: {
+                                        properties: {
+                                            metadata: metadata
+                                        }
+                                    }
+                                }));
+                            }
+                        };
+
+                        let subscribeReactStore = store => {
+                            this._subscription = store.subscribe({
+                                effect: state => {
+                                    parseReactState(state);
+                                    return {
+                                        result: 1
+                                    };
+                                }
+                            });
+                        };
+
+                        let props = outlineContainer.getReactProps();
+                        if (props) {
+                            subscribeReactStore(props.store);
+                        } else {
+                            let oldRenderReactComponent = outlineContainer.renderReactComponent;
+                            outlineContainer.renderReactComponent = e => {
+                                let props = outlineContainer.getReactProps();
+                                subscribeReactStore(props.store);
+
+                                oldRenderReactComponent.call(outlineContainer, e);
+                            }
+                        }
+                    }
+                }
+            } catch (e) {}
+        }
+
+        disconnectedCallback() {
+            if (this._subscription) { // react store subscription
+                this._subscription();
+                this._subscription = null;
+            }
+        }
+
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            if ("designMode" in changedProperties) {
+                this._designMode = changedProperties["designMode"];
+            }
+        }
+
+        onCustomWidgetAfterUpdate(changedProperties) {
+            var that = this;
+            loadthis(that, changedProperties);
+        }
+
+        _renderExportButton() {
+            let components = this.metadata ? JSON.parse(this.metadata)["components"] : {};
+        }
+
+        _firePropertiesChanged() {
+            this.unit = "";
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: {
+                        unit: this.unit
+                    }
+                }
+            }));
+        }
+
+        // SETTINGS
+        get title() {
+            return this._export_settings.title;
+        }
+        set title(value) {
+            this._export_settings.title = value;
+        }
+
+        get subtitle() {
+            return this._export_settings.subtitle;
+        }
+        set subtitle(value) {
+            this._export_settings.subtitle = value;
+        }
+
+        get icon() {
+            return this._export_settings.icon;
+        }
+        set icon(value) {
+            this._export_settings.icon = value;
+        }
+
+        get unit() {
+            return this._export_settings.unit;
+        }
+        set unit(value) {
+            value = _score;
+            this._export_settings.unit = value;
+        }
+
+        get footer() {
+            return this._export_settings.footer;
+        }
+        set footer(value) {
+            this._export_settings.footer = value;
+        }
+
+        static get observedAttributes() {
+            return [
+                "title",
+                "subtitle",
+                "icon",
+                "unit",
+                "footer",
+                "link"
+            ];
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (oldValue != newValue) {
+                this[name] = newValue;
+            }
+        }
+
+    }
+    customElements.define("com-fd-djaja-sap-sac-multiinput", MultiInput);
+
+    // UTILS
+    function loadthis(that, changedProperties) {
+        var that_ = that;
+
+        widgetName = changedProperties.widgetName;
+        if (typeof widgetName === "undefined") {
+            widgetName = that._export_settings.title.split("|")[0];
+        }
+
+        div = document.createElement('div');
+        div.slot = "content_" + widgetName;
+
+        let div0 = document.createElement('div');
+        div0.innerHTML = '<?xml version="1.0"?><script id="oView_' + widgetName + '" name="oView_' + widgetName + '" type="sapui5/xmlview"><mvc:View height="100%" xmlns="sap.m" xmlns:l="sap.ui.layout" xmlns:core="sap.ui.core" xmlns:mvc="sap.ui.core.mvc" controllerName="myView.Template"><l:VerticalLayout class="sapUiContentPadding" width="100%"><l:content><MultiInput width="100%" id="multiInput" suggestionItems="{' + widgetName + '>/}" valueHelpRequest="handleValueHelp"><core:Item key="{' + widgetName + '>partner}" text="{' + widgetName + '>partner}" /></MultiInput></l:content><Button id="buttonId" class="sapUiSmallMarginBottom" text="Get Score" width="150px" press=".onButtonPress" /></l:VerticalLayout></mvc:View></script>';
+        _shadowRoot.appendChild(div0);
+
+        let div1 = document.createElement('div');
+        div1.innerHTML = '<?xml version="1.0"?><script id="myXMLFragment_' + widgetName + '" type="sapui5/fragment"><core:FragmentDefinition xmlns="sap.m" xmlns:core="sap.ui.core"><SelectDialog title="Partner Number" class="sapUiPopupWithPadding"  items="{' + widgetName + '>/}" search="_handleValueHelpSearch"  confirm="_handleValueHelpClose"  cancel="_handleValueHelpClose"  multiSelect="true" showClearButton="true" rememberSelections="true"><StandardListItem icon="{' + widgetName + '>ProductPicUrl}" iconDensityAware="false" iconInset="false" title="{' + widgetName + '>partner}" description="{' + widgetName + '>partner}" /></SelectDialog></core:FragmentDefinition></script>';
+        _shadowRoot.appendChild(div1);
+
+        let div2 = document.createElement('div');
+        div2.innerHTML = '<div id="ui5_content_' + widgetName + '" name="ui5_content_' + widgetName + '"><slot name="content_' + widgetName + '"></slot></div>';
+        _shadowRoot.appendChild(div2);
+
+        that_.appendChild(div);
+
+        var mapcanvas_divstr = _shadowRoot.getElementById('oView_' + widgetName);
+        var mapcanvas_fragment_divstr = _shadowRoot.getElementById('myXMLFragment_' + widgetName);
+
+        Ar.push({
+            'id': widgetName,
+            'div': mapcanvas_divstr,
+            'divf': mapcanvas_fragment_divstr
+        });
+
+        that_._renderExportButton();
+
+        sap.ui.getCore().attachInit(function() {
+            "use strict";
+
+            //### Controller ###
+            sap.ui.define([
+                "jquery.sap.global",
+                "sap/ui/core/mvc/Controller",
+                "sap/ui/model/json/JSONModel",
+                "sap/m/MessageToast",
+                "sap/ui/core/library",
+                "sap/ui/core/Core",
+                'sap/ui/model/Filter',
+                'sap/m/library',
+                'sap/m/MessageBox',
+                'sap/ui/unified/DateRange',
+                'sap/ui/core/format/DateFormat',
+                'sap/ui/model/BindingMode',
+                'sap/ui/core/Fragment',
+                'sap/m/Token',
+                'sap/ui/model/FilterOperator',
+                'sap/ui/model/odata/ODataModel',
+                'sap/m/BusyDialog'
+            ], function(jQuery, Controller, JSONModel, MessageToast, coreLibrary, Core, Filter, mobileLibrary, MessageBox, DateRange, DateFormat, BindingMode, Fragment, Token, FilterOperator, ODataModel, BusyDialog) {
+                "use strict";
+
+                var busyDialog = (busyDialog) ? busyDialog : new BusyDialog({});
+
+                return Controller.extend("myView.Template", {
+
+                    onInit: function() {
+
+                        if (that._firstConnection === 0) {
+                            that._firstConnection = 1;
+                        } else {
+                            var _oModel = new JSONModel(JSON.parse(that._export_settings.footer.split("|")[1]));
+
+                            _oModel.setSizeLimit(1000000);
+
+                            this.getView()
+                                .setModel(_oModel, that.widgetName);
+
+                            sap.ui.getCore().setModel(_oModel, that.widgetName);
+                        }
+                    },
+
+                    handleValueHelp: function(oEvent) {
+                        var sInputValue = oEvent.getSource().getValue();
+
+                        if (!this._valueHelpDialog) {
+
+                            var foundIndex_ = Ar.findIndex(x => x.id == widgetName);
+                            var divfinal_ = Ar[foundIndex].divf;
+
+                            this._valueHelpDialog = sap.ui.xmlfragment({
+                                fragmentContent: jQuery(divfinal_).html()
+                            }, this);
+
+                            var oModel = sap.ui.getCore().getModel(widgetName);
+
+                            this._valueHelpDialog.setModel(oModel);
+                            this.getView().addDependent(this._valueHelpDialog);
+                        }
+                        this._openValueHelpDialog(sInputValue);
+                    },
+
+                    _openValueHelpDialog: function(sInputValue) {
+                        // create a filter for the binding
+                        this._valueHelpDialog.getBinding("items").filter([new Filter(
+                            "partner",
+                            FilterOperator.Contains,
+                            sInputValue
+                        )]);
+
+                        // open value help dialog filtered by the input value
+                        this._valueHelpDialog.open(sInputValue);
+                    },
+
+                    _handleValueHelpSearch: function(evt) {
+                        var sValue = evt.getParameter("value");
+                        var oFilter = new Filter(
+                            "partner",
+                            FilterOperator.Contains,
+                            sValue
+                        );
+                        evt.getSource().getBinding("items").filter([oFilter]);
+                    },
+
+                    _handleValueHelpClose: function(evt) {
+                        var aSelectedItems = evt.getParameter("selectedItems"),
+                            oMultiInput = this.byId("multiInput");
+
+                        if (aSelectedItems && aSelectedItems.length > 0) {
+                            aSelectedItems.forEach(function(oItem) {
+                                oMultiInput.addToken(new Token({
+                                    text: oItem.getTitle()
+                                }));
+                            });
+                        }
+                    },
+
+                    onButtonPress: async function(oEvent) {
+                        var this_ = this;
+                        this_.wasteTime();
+                        var oMultiInput = this.byId("multiInput");
+
+                        var aTokens = oMultiInput.getTokens();
+                        // Get an array of the keys of the tokens and join them together with a "," inbetween
+                        var sData = aTokens.map(function(oToken) {
+                            return oToken.getText();
+                        }).join(",");
+
+                        console.log(sData);
+
+                        _score = sData;
+
+                        that._firePropertiesChanged();
+                        this.settings = {};
+                        this.settings.score = "";
+
+                        that.dispatchEvent(new CustomEvent("onStart", {
+                            detail: {
+                                settings: this.settings
+                            }
+                        }));
+
+                        this_.runNext();
+                    },
+
+                    onExit: function() {
+                        if (this._oQuickView) {
+                            this._oQuickView.destroy(true);
+                        }
+                    },
+
+                    wasteTime: function() {
+                        busyDialog.open();
+                    },
+
+                    runNext: function() {
+                        busyDialog.close();
+                    },
+
+                });
+            });
+
+            console.log("widgetName Final:" + widgetName);
+            var foundIndex = Ar.findIndex(x => x.id == widgetName);
+            var divfinal = Ar[foundIndex].div;
+            console.log(divfinal);
+
+            //### THE APP: place the XMLView somewhere into DOM ###
+            var oView = sap.ui.xmlview({
+                viewContent: jQuery(divfinal).html(),
+            });
+
+            oView.placeAt(div);
+            if (that_._designMode) {
+                oView.byId("multiInput").setEnabled(false);
+                oView.byId("buttonId").setEnabled(false);
+            }
+        });
+    }
+
+    function createGuid() {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+            let r = Math.random() * 16 | 0,
+                v = c === "x" ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    function loadScript(src, shadowRoot) {
+        return new Promise(function(resolve, reject) {
+            let script = document.createElement('script');
+            script.src = src;
+
+            script.onload = () => {
+                console.log("Load: " + src);
+                resolve(script);
+            }
+            script.onerror = () => reject(new Error(`Script load error for ${src}`));
+
+            shadowRoot.appendChild(script)
+        });
+    }
+})();
